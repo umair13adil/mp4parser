b695cc4a0151cc11e24452680c75f75b4178ba53
Umair_Adil <umair_adil@live.com>
2016-07-25 16:59:56 +0500

    Audio merger working with AAC Audio & MP4 Video Files

---
+++ app/src/main/java/com/umair_adil/audio_video_merger/mp3agic/InvalidDataException.java
@@ -0,0 +1,18 @@
+package com.umair_adil.audio_video_merger.mp3agic;
+
+public class InvalidDataException extends BaseException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public InvalidDataException() {
+		super();
+	}
+	
+	public InvalidDataException(String message) {
+		super(message);
+	}
+	
+	public InvalidDataException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
